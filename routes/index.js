e04dd1e4eedfbef21d12494b5fbeff07c19714e0
Trung Hai Nguyen <trunghaing2912@gmail.com>
2024-03-29 14:04:24 +0700

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Good Morning, How Are You' });
 });
 
 module.exports = router;
